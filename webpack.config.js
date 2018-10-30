5772256b03015863900f62d5bffa3c0cbc520628
Eyal Lapid <elpddev@users.noreply.github.com>
2018-10-30 22:40:14 +0200

    build(typescript): add typescript build support
    issue#3

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path');
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin');
 
 module.exports = {
   mode: 'development',
@@ -7,4 +8,31 @@ module.exports = {
     filename: 'restangular.js',
     path: path.resolve(__dirname, 'dist'),
   },
+  module: {
+      rules: [{
+          test: /\.tsx?$/,
+          use: [
+              { loader: 'cache-loader' },
+              {
+                  loader: 'thread-loader',
+                  options: {
+                      // there should be 1 cpu for the fork-ts-checker-webpack-plugin
+                      workers: require('os').cpus().length - 1,
+                  },
+              },
+              {
+                  loader: 'ts-loader',
+                  options: {
+                      happyPackMode: true // IMPORTANT! use happyPackMode mode to speed-up compilation and reduce errors reported to webpack
+                  }
+              }
+          ]
+      }]
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+  plugins: [
+    new ForkTsCheckerWebpackPlugin({ checkSyntacticErrors: true })
+  ]
 };
